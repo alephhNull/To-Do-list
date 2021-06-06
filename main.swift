145162ce68b28b6e567ee8a588cbeb9f2763f9b7
Saleh-Safarnejad <saleh.safarnejad@gmail.com>
2021-06-06 22:46:59 +0430

    initialize structure

---
+++ main.swift
@@ -0,0 +1,86 @@
+
+MyApp.mainMenu()
+
+class MyApp {
+    static var lists = [List]()
+
+    static func mainMenu() {
+        while true {
+            print("\nEnter one option below: \n" +
+            "1. Create new list \n" +
+            "2. See exisiting lists \n" +
+            "3. Categories \n" + 
+            "4. Quit")
+            let command = readLine()
+            switch command {
+            case "1":
+                print("\nEnter name of the new list:")
+                let name = readLine()
+                let list = List(name: name!) 
+                lists.append(list)
+                listMenu(list: list)
+            case "2":
+                break
+            case "3":
+                break
+            case "4":
+                return
+            default:
+                break
+            }
+        }           
+    }
+
+    static func listMenu(list: List) {
+        //TODO
+        while true {
+            print("\nList: \(list.name)")
+            print("items:")
+            for item in list.items {
+                print(item)
+            }
+            print("\n1. Add item\n" +
+            "2. Edit item\n" +
+            "3. Remove item\n" +
+            "4. Sort items\n" + 
+            "5. back")
+            break
+        }
+    }
+}
+
+class List {
+    var name: String
+    var items = [Item]()
+
+    init(name: String) {
+        self.name = name
+    }
+    
+}
+
+class Item: CustomStringConvertible {
+    var title: String
+    var content: String
+    var priority: Int
+    
+    init(title: String, content: String, priority: Int) {
+        self.title = title
+        self.content = content
+        self.priority = priority
+    }
+
+    var description: String {
+        return "\ntitle: \(self.title) \n" +
+        "content: \(self.content) \n" +
+        "priority: \(self.priority)"
+    }
+}
+
+class Category:List {
+
+    override init(name: String) {
+        super.init(name: name)
+    }
+    
+}
